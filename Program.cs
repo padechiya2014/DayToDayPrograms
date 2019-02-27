7ad0ad6e23b4d1512b14079163cd35b55a1d0b0a
Mihir Padechiya <31804739+padechiya2014@users.noreply.github.com>
2019-02-27 16:16:13 -0500

    New Year Chaos
    It's New Year's Day and everyone's in line for the Wonderland rollercoaster ride! There are a number of
    people queued up, and each person wears a sticker indicating their initial position in the queue. Initial
    positions increment by from at the front of the line to at the back.
    Any person in the queue can bribe the person directly in front of them to swap positions. If two people
    swap positions, they still wear the same sticker denoting their original places in line. One person can bribe
    at most two others . For example, if and bribes , the queue will look like this:
    .
    Fascinated by this chaotic queue, you decide you must know the minimum number of bribes that took
    place to get the queue into its current state!
    Function Description
    Complete the function minimumBribes in the editor below. It must print an integer representing the
    minimum number of bribes necessary, or Too chaotic if the line configuration is not possible.
    minimumBribes has the following parameter(s):
    q: an array of integers
    Input Format
    The first line contains an integer , the number of test cases.
    Each of the next pairs of lines are as follows:
    - The first line contains an integer , the number of people in the queue
    - The second line has space-separated integers describing the final state of the queue.
    Constraints
    Subtasks
    For score
    For score
    Output Format
    Print an integer denoting the minimum number of bribes needed to get the queue into its final state. Print
    Too chaotic if the state is invalid, i.e. it requires a person to have bribed more than people.
    Sample Input
    252
    1 5 3 4
    52
    5 1 3 4
    Sample Output
    3Too chaotic
    Explanation
    Test Case 1
    The initial state:
    After person moves one position ahead by bribing person :
    Now person moves another position ahead by bribing person :
    And person moves one position ahead by bribing person :
    So the final state is after three bribing operations.
    Test Case 2
    No person can bribe more than two people, so its not possible to achieve the input state.

--- Program.cs
+++ Program.cs
@@ -1,203 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LinkedList
+namespace Arrays
 {
     class Program
     {
+        static void minimumBribes(int[] q)
+        {
+            // Loop to check whether chaotic, if yes, than print and break out
+            for (int i = 0; i < q.Length; i++)
+            {
+                if ((q[i] - (i + 1)) > 2)
+                {
+                    Console.WriteLine("Too chaotic");
+                    return;
+                }
+            }
+
+            int result = 0;
+            int[] initialArray = new int[q.Length];
+            for (int x = 0; x < initialArray.Length; x++)
+            {
+                initialArray[x] = x + 1;
+            }
+            // If not chaotic, than check the number of bribes(swaps) by bringing the position of the queue to its original position
+            // for eg: 1 2 3 4 5 6 7 8....etc
+            for (int i = 0; i < q.Length; i++)
+            {
+                if (q[i] == initialArray[i])
+                {
+                    continue;
+                }
+                for (int j = i + 1; j < q.Length; j++)
+                {
+
+                    if (q[i] > q[j])
+                    {
+                        int tmp = q[j];
+                        q[j] = q[i];
+                        q[i] = tmp;
+                        result++;
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+
         static void Main(string[] args)
         {
-            // Case 1 - Find middle element
-            FindMiddleElement llist = new FindMiddleElement();
-            for (int i = 5; i > 0; --i)
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
             {
-                llist.push(i);
-                llist.printList();
-                llist.printMiddle();
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int[] q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp))
+                ;
+                minimumBribes(q);
             }
-
-
-            //// Case 2 - Delete the first occurence
-            DeleteFirstOccurence dlList = new DeleteFirstOccurence();
-
-            dlList.push(7);
-            dlList.push(1);
-            dlList.push(3);
-            dlList.push(2);
-
-            Console.WriteLine("\nCreated Linked list is:");
-            Console.ReadLine();
-            dlList.printList();
-
-            dlList.deleteNode(1); // Delete node at position 4
-
-            Console.WriteLine("\nLinked List after Deletion at position 4:");
-            Console.ReadLine();
-            dlList.printList();
-
-
-            // Case 3 - Delete all the occurences
-            DeleteFirstOccurence dlListCase3 = new DeleteFirstOccurence();
-            dlList.push(7);
-            dlList.push(2);
-            dlList.push(3);
-            dlList.push(2);
-            dlList.push(8);
-            dlList.push(1);
-            dlList.push(2);
-            dlList.push(2);
-            dlList.push(4);
-            dlList.push(5);
-            dlList.push(4);
-            dlList.push(4);
-            dlList.push(4);
-            dlList.push(3);
-            dlList.push(2);
-            dlList.push(1);
-
-            Console.WriteLine("\nCreated Linked list is:");
-            Console.ReadLine();
-            dlList.printList();
-
-            dlList.deleteAllMatchingNodes(4); // Delete node at position 4
-
-            Console.WriteLine("\nLinked List after Deletion at position 4:");
-            Console.ReadLine();
-            dlList.printList();
-
-
-            // Case 4 - Delete the last element
-            DeleteFirstOccurence dlListCase4 = new DeleteFirstOccurence();
-            dlList.push(4);
-            dlList.push(5);
-            dlList.push(4);
-            dlList.push(4);
-            dlList.push(4);
-            dlList.push(3);
-            dlList.push(2);
-            dlList.push(1);
-
-            Console.WriteLine("\nCreated Linked list is:");
-            Console.ReadLine();
-            dlList.printList();
-
-            dlList.DeleteLastElemnt(); // Delete node at position 4
-
-            Console.WriteLine("\nLinked List after Deletion of last element:");
-            Console.ReadLine();
-            dlList.printList();
-
-            // Case 5 - Print the reverse of a Linked List
-            Reverse ReverseList = new Reverse();
-            ReverseList.push(4);
-            ReverseList.push(5);
-            ReverseList.push(4);
-            ReverseList.push(4);
-            ReverseList.push(4);
-            ReverseList.push(3);
-            ReverseList.push(2);
-            ReverseList.push(1);
-
-            Console.WriteLine("\nCreated Linked list is:");
-            Console.ReadLine();
-            ReverseList.printList();
-
-            ReverseList.printReverse();
-
-            // Case 6 - Delete the Last occurence of the given key
-            DeleteLastOccurence delLastOccList = new DeleteLastOccurence();
-            delLastOccList.push(4);
-            delLastOccList.push(4);
-            delLastOccList.push(5);
-            delLastOccList.push(4);
-            delLastOccList.push(3);
-            delLastOccList.push(2);
-            delLastOccList.push(1);
-
-            Console.WriteLine("\nCreated Linked list is:");
-            Console.ReadLine();
-            delLastOccList.printList();
-
-            delLastOccList.deleteLastOccuringNode(4);
-
-            Console.WriteLine("\nCreated Linked list is:");
-            Console.ReadLine();
-            delLastOccList.printList();
-
-            // Case 7 - Detect a loop in a singly Linked List
-            DetectLoop dtLoop = new DetectLoop();
-            //dtLoop.push(10);
-            dtLoop.push(20);
-            dtLoop.push(4);
-            dtLoop.push(15);
-            dtLoop.push(10);
-
-            // Create a loop
-            dtLoop.Head.Next.Next.Next.Next = dtLoop.Head;
-
-            if (dtLoop.detectLoopUsingDictionary(dtLoop.Head))
-            {
-                Console.WriteLine("Loop found");
-                Console.ReadLine();
-            }
-            if (dtLoop.detectLoopUsingSlowFastPointers(dtLoop.Head))
-            {
-                Console.WriteLine("Loop found");
-                Console.ReadLine();
-            }
-
-            else
-            {
-                Console.WriteLine("No Loop");
-                Console.ReadLine();
-            }
-
-
-            // Case 8 - Detect and Remove a loop
-            dtLoop.DetectAndRemoveLoop(dtLoop.Head);
-
-            // Case 9 - Rotate Linked List
-            RotateList rotateList = new RotateList();
-            rotateList.push(60);
-            rotateList.push(50);
-            rotateList.push(40);
-            rotateList.push(30);
-            rotateList.push(20);
-            rotateList.push(10);
-
-
-            Console.WriteLine("\nCreated Linked list is:");
-            Console.ReadLine();
-            rotateList.printList();
-
-            rotateList.RotateLinkedList(4);
-
-            Console.WriteLine("\nRotated Linked list is:");
-            Console.ReadLine();
-            rotateList.printList();
-
-            // Case 10 - Rotate and reverse a linked list
-            RotateList rotateListCase10 = new RotateList();
-            rotateList.push(8);
-            rotateList.push(7);
-            rotateList.push(6);
-            rotateList.push(5);
-            rotateList.push(4);
-            rotateList.push(3);
-            rotateList.push(2);
-            rotateList.push(1);
-            Console.WriteLine("\nCreated Linked list is:");
-            Console.ReadLine();
-            rotateList.printList();
-
-            rotateList.ReverseAndRotateList(3);
-
-            Console.WriteLine("\nRotated Linked list is:");
-            Console.ReadLine();
-            rotateList.printList();
         }
     }
 }
