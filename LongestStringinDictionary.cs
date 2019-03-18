e09b292628bfb80f4e0add59708f269eb202be1a
Mihir Padechiya <31804739+padechiya2014@users.noreply.github.com>
2019-03-18 16:24:20 -0400

    Find largest word in dictionary by deleting some characters of given string
    Giving a dictionary and a string ‘str’, find the longest string in the dictionary which can be formed by deleting some characters of the given ‘str’.
    Input : dict = {"ale", "apple", "monkey", "plea"}   
            str = "abpcplea"  
    Output : apple 
    
    Input  : dict = {"pintu", "geeksfor", "geeksgeeks", 
                                            " forgeek"} 
             str = "geeksforgeeks"
    Output : geeksgeeks

---
+++ LongestStringinDictionary.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringManipulation
+{
+    class LongestStringinDictionary
+    {
+        // Returns true if str1[] is a  
+        // subsequence of str2[]. m is  
+        // length of str1 and n is length of str2  
+        static bool IsSubSequence(string str1, string str2)
+        {
+            int m = str1.Length, n = str2.Length;
+
+            int j = 0; // For index of str1 (or subsequence)  
+
+            // Traverse str2 and str1, and compare current  
+            // character of str2 with first unmatched char  
+            // of str1, if matched then move ahead in str1  
+            for (int i = 0; i < n && j < m; i++)
+            {
+                if (str1.ElementAt(j) == str2.ElementAt(i))
+                {
+                    j++;
+                }
+            }
+
+            // If all characters of str1  
+            // were found in str2  
+            return (j == m);
+        }
+
+        // Returns the longest String  
+        // in dictionary which is a  
+        // subsequence of str.  
+        static string findLongestString(List<string> dict,
+                                                string str)
+        {
+            string result = "";
+            int length = 0;
+
+            // Traverse through all words of dictionary  
+            foreach (var word in dict)
+            {
+                // If current word is subsequence of str  
+                // and is largest such word so far.  
+                if (length <= word.Length &&
+                    IsSubSequence(word, str))
+                {
+                    result = word;
+                    length = word.Length;
+                }
+            }
+
+
+            // Return longest String  
+            return result;
+        }
+
+        // Driver code 
+        public static void Main(string[] args)
+        {
+            string[] arr = { "ale", "apple", "monkey", "plea" };
+            List<string> dict = arr.ToList();
+            string str = "abpcplea";
+            Console.WriteLine(findLongestString(dict, str));
+            Console.ReadLine();
+        }
+    }
+
+}
