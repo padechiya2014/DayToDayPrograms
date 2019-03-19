a6e683057aa970ffc99ef68da9a8414cbde7e11d
Mihir Padechiya <31804739+padechiya2014@users.noreply.github.com>
2019-03-19 16:51:56 -0400

    Maximum sum such that no two elements are adjacent
    Given an array of positive numbers, find the maximum sum of a subsequence with the constraint that no 2 numbers in the sequence should be adjacent in the array. So 3 2 7 10 should return 13 (sum of 3 and 10) or 3 2 5 10 7 should return 15 (sum of 3, 5 and 7).Answer the question in most efficient way.

---
+++ MaxSumForNonAdjacentElements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Arrays
+{
+    class MaxSumForNonAdjacentElements
+    {
+        static int MaxSum(int[] arr)
+        {
+            int excl = 0;
+            int incl = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int temp = incl;
+                incl = Math.Max(excl + arr[i], incl);
+                excl = temp;
+            }
+            return incl;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 2, 10, 13, 4, 2, 15, 10 };
+            Console.WriteLine(MaxSum(arr));
+            Console.ReadLine();
+        }
+    }
+}
