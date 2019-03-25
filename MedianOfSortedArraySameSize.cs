a30af3fcaf8d733dde487e979924b944b32e8858
Mihir Padechiya <31804739+padechiya2014@users.noreply.github.com>
2019-03-25 13:53:45 -0400

    Median of two sorted arrays of same size
    There are 2 sorted arrays A and B of size n each. Write an algorithm to find the median of the array obtained after merging the above 2 arrays(i.e. array of length 2n)

---
+++ MedianOfSortedArraySameSize.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Arrays
+{
+    class MedianOfSortedArraySameSize
+    {
+        public static int getMedian(int[] arr1, int[] arr2, int length)
+        {
+            int size = length;
+            int median1 = -1;
+            int median2 = -1;
+            int i = 0;
+            int j = 0;
+
+
+            for (int count = 0; count <= size; count++)
+            {
+                // Edge case 1
+                if (i == size)
+                {
+                    median1 = median2;
+                    median2 = arr2[0];
+                    break;
+                }
+
+                // Edge case 2
+                else if (j == size)
+                {
+                    median1 = median2;
+                    median2 = arr1[0];
+                    break;
+                }
+
+                if (arr1[i] < arr2[j])
+                {
+                    median1 = median2;
+                    median2 = arr1[i];
+                    i++;
+                }
+                else
+                {
+                    median1 = median2;
+                    median2 = arr2[j];
+                    j++;
+                }
+
+            }
+
+            return (median1 + median2) / 2;
+
+        }
+
+        public static void Main()
+        {
+            int[] ar1 = { 1, 12, 15, 26, 38 };
+            int[] ar2 = { 2, 13, 17, 30, 45 };
+
+            int n1 = ar1.Length;
+            int n2 = ar2.Length;
+            if (n1 == n2)
+            {
+                Console.WriteLine("Median is " + getMedian(ar1, ar2, n1));
+            }
+
+            else
+            {
+                Console.WriteLine("arrays are of unequal size");
+            }
+
+        }
+    }
+}
