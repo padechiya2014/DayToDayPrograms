ac7a8247bc412a0866e927d80acc5afe0f840bd5
Mihir Padechiya <31804739+padechiya2014@users.noreply.github.com>
2019-03-28 00:00:34 -0400

    count the frequency of each element of an array
    Write a program in C# Sharp to count the frequency of each element of an array
     int[] arr = { 1, 2, 2, 2, 3, 3, 4, 5, 8, 9, 9, 9, 9, 10 };

---
+++ CountFreqOfANumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class CountFreqOfANumber
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 2, 2, 3, 3, 4, 5, 8, 9, 9, 9, 9, 10 };
+
+            Dictionary<int, int> FreqDict = new Dictionary<int, int>();
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int counter = 0;
+                if (FreqDict.ContainsKey(arr[i]))
+                {
+                    FreqDict[arr[i]] = ++FreqDict[arr[i]];
+                }
+                else
+                {
+                    FreqDict.Add(arr[i], ++counter);
+                }
+               
+            }
+
+            foreach (KeyValuePair<int,int> item in FreqDict)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+        }
+    }
+}
