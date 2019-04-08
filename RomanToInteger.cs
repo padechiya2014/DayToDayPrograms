3167000d780b1848975b626355745f760ee826e9
Mihir Padechiya <31804739+padechiya2014@users.noreply.github.com>
2019-04-08 14:09:41 -0400

    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
    Symbol       Value
    I             1
    V             5
    X             10
    L             50
    C             100
    D             500
    M             1000
    For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
    
    Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
    
    I can be placed before V (5) and X (10) to make 4 and 9. 
    X can be placed before L (50) and C (100) to make 40 and 90. 
    C can be placed before D (500) and M (1000) to make 400 and 900.

---
+++ RomanToInteger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    class RomanToInteger
+    {
+
+        public static void Main(string[] args)
+        {
+            RomanToInteger x = new RomanToInteger();
+            string v = "III";
+
+            x.RomanToInt(v);
+
+        }
+        readonly Dictionary<char, int> dicRomInt = new Dictionary<char, int>();
+
+        public int RomanToInt(string s)
+        {
+            dicRomInt.Add('I', 1);
+            dicRomInt.Add('V', 5);
+            dicRomInt.Add('X', 10);
+            dicRomInt.Add('L', 50);
+            dicRomInt.Add('C', 100);
+            dicRomInt.Add('D', 500);
+            dicRomInt.Add('M', 1000);
+
+            char[] tempArr = s.ToCharArray();
+            int result = 0;
+
+            for (int i = 0; i < tempArr.Length; i++)
+            {
+
+                if (dicRomInt.ContainsKey(tempArr[i]))
+                {
+                    if((i + 1) < tempArr.Length && dicRomInt[tempArr[i]] >= dicRomInt[tempArr[i + 1]])
+                    {
+                        result += dicRomInt[tempArr[i]];
+                    }
+                    else if((i + 1) < tempArr.Length && dicRomInt[tempArr[i + 1]] > dicRomInt[tempArr[i]])
+                    {
+                        int temp = dicRomInt[tempArr[i + 1]] - dicRomInt[tempArr[i]];
+                        result += temp;
+                        i++;
+                    }
+                    else
+                    {
+                        result += dicRomInt[tempArr[i]];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
