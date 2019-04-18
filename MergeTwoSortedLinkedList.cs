a792d822cbb3c4ae3df370104a26d1ed2b32b35f
Mihir Padechiya <31804739+padechiya2014@users.noreply.github.com>
2019-04-18 19:04:51 -0400

    Merge Two Sorted Lists
    Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
    
    Example:
    
    Input: 1->2->4, 1->3->4
    Output: 1->1->2->3->4->4

---
+++ MergeTwoSortedLinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class ListNode
+    {
+      public int val;
+      public ListNode next;
+     public ListNode(int x) { val = x; }
+  }
+
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+
+            ListNode list1 = l1;
+            ListNode list2 = l2;
+
+            if (list1 == null && list2 == null)
+            {
+                return list1;
+            }
+            List<ListNode> arrList = new List<ListNode>();
+
+            while (list1 != null)
+            {
+                arrList.Add(list1);
+                list1 = list1.next;
+            }
+
+            while (list2 != null)
+            {
+                arrList.Add(list2);
+                list2 = list2.next;
+            }
+
+            List<ListNode> SortedList = arrList.OrderBy(o => o.val).ToList();
+            ListNode head = new ListNode(SortedList[0].val);
+            ListNode temp = head;
+            for (int i = 1; i < SortedList.Capacity; i++)
+            {
+                ListNode newNode = new ListNode(SortedList[i].val);
+                temp.next = newNode;
+                temp = newNode;
+
+            }
+
+            return head;
+        }
+    }
+}
