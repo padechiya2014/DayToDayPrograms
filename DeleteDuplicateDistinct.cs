ef43bcca0dbb94c1421a71bf12f990b2be2c4642
Mihir Padechiya <31804739+padechiya2014@users.noreply.github.com>
2019-10-20 12:52:07 -0400

    Remove Duplicates from Sorted List II
    Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
    
    Example 1:
    
    Input: 1->2->3->3->4->4->5
    Output: 1->2->5
    Example 2:
    
    Input: 1->1->1->2->3
    Output: 2->3

---
+++ DeleteDuplicateDistinct.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class DeleteDuplicateDistinct
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode current = head;
+            ListNode prev = head;
+            bool flag = false;
+
+
+            while (current != null && current.next != null)
+            {
+
+                if (current.val == current.next.val)
+                {
+
+                    flag = true;
+                    current.next = current.next.next;
+
+                }
+                else if (flag == true)
+                {
+                    if (prev == head)
+                    {
+                        head = head.next;
+                    }
+                    current = prev;
+                    current.next = current.next.next;
+                    flag = false;
+                }
+
+                else
+                {
+                    prev = current;
+                    current = current.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
